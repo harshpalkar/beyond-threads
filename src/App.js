bf567ff385ce1d3dae5b1a4302b83902191ffdac
Harsh Palkar <explorewitharsh@outlook.com>
2022-11-29 18:11:05 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
+import "./categories/categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
       id: 1,
       title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      id: 1,
+      id: 2,
       title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      id: 1,
+      id: 3,
       title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      id: 1,
+      id: 4,
       title: "Men",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
     {
-      id: 1,
+      id: 5,
       title: "Women",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
-          {/* {img} */}
+      {categories.map(({ title, id, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          ></div>
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
