73099546c92eaf566615172f978a1d2b9a1d4e00
Harsh Palkar <explorewitharsh@outlook.com>
2022-12-01 16:54:35 +0530

    routing-lesson

--- src/routes/navigation/Navigation.component.jsx
+++ src/routes/navigation/Navigation.component.jsx
@@ -1,16 +1,29 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
+import "./Navigation.styles.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="nav-bar">
         <Link className="nav-logo-link" to="/">
-          <div>Logo</div>
+          <div>
+            <img
+              className="beyond-threads-logo"
+              src={require("../../assets/image2.png")}
+              alt="logo"
+            ></img>
+          </div>
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
+          </Link>
+          <Link className="nav-link" to="/contact">
+            CONTACT
+          </Link>
+          <Link className="nav-link" to="/signup">
+            SIGN IN
           </Link>
         </div>
       </div>
